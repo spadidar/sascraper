1d7ba8c4f4bebc3eec4b81385faf1fbc49383143
Sasan <siesonz@gmail.com>
2011-10-04 23:58:36 -0700

    with http:// request works

--- http_utilities.erl
+++ http_utilities.erl
@@ -1,7 +1,13 @@
 -module(http_utilities).
 
--export([get/1]).
+-export([start_inet/0,
+	 get/1]).
 
 start_inet()->
-    start_inet().
+    inets:start().
+
+get(URL) ->
+    io:format("Get " ++ URL ++ "~n" ),
+    { ok, {Status, Headers, Body }} = http:request(URL),
+    Body.
 
