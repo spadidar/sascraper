03ca95895b4ae7243224961a3d3d999a8b673674
Sasan <siesonz@gmail.com>
2012-01-24 16:39:42 -0800

    added graham

--- src/run.erl
+++ src/run.erl
@@ -4,7 +4,8 @@
 
 run_app() -> 
     main:start("",""),
-    R = http_utils:get("http://www.tamale.net/erlang/tutorial.shtml"),
+    R = http_utils:get("http://paulgraham.com/better.html"),
+    %% R = http_utils:get("http://www.tamale.net/erlang/tutorial.shtml"),
     T = http_utils:parse_response(R),
     http_utils:extract_text(T),
     %% io:format("~p~n", [T]),
