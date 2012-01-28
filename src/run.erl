40b298805415e5f4d5a76a5cfeb5d7e47260276a
Sasan <siesonz@gmail.com>
2012-01-27 16:50:17 -0800

    file write adede

--- src/run.erl
+++ src/run.erl
@@ -5,6 +5,7 @@
 run_app() -> 
     main:start("",""),
     R = http_utils:get("http://www.tamale.net/erlang/tutorial.shtml"),
+    file:write_file("./response.txt", io_lib:fwrite("~p.\n", [R])),
     T = http_utils:parse_response(R),
     http_utils:extract_text(T),
     %% io:format("~p~n", [T]),
