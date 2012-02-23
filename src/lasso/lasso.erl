3e49c85ce1881a13ce2bfa67b4d5ee0096d5b629
Sasan <siesonz@gmail.com>
2012-02-23 00:04:55 -0800

    added regex

--- src/lasso/lasso.erl
+++ src/lasso/lasso.erl
@@ -2,7 +2,9 @@
 -export([download/1, 
 	 store_response/1,
 	 scrape_urls/1,
-	 scrape/3]).
+	 scrape/3,
+	 extract_urls/1
+	]).
 
 scrape(URL, Depth, Speed) ->
     .
@@ -10,8 +12,20 @@ scrape(URL, Depth, Speed) ->
 scrape_urls(URL) ->
     case download(URL) of
 	{ok, {{StatusCode, ReasonPhrase}, Hdrs, ResponseBody}} ->
-	    
+	    extract_urls(ResponseBody). 
     end.
+
+extract_urls(Response) ->
+    RegExp = "href=[\"]?([^\">]+)",
+    Options = [],
+    {ok, RE} = re:compile(RegExp),
+    case re:run(Response, RE) of
+	{match, Result} -> 
+	    Result;
+	nomatch -> 
+	    nomatch
+	end.
+
 
 download(URL) ->
     case htt_utils:get(URL) of
