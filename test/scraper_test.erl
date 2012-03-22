4968d2cd9a3f0d57021bb07cb277393cb4485d0c
Sasan <siesonz@gmail.com>
2012-03-22 08:41:05 -0700

    small test

--- test/scraper_test.erl
+++ test/scraper_test.erl
@@ -1,3 +1,9 @@
 -module(scraper_test).
 -compile(export_all).
 -include_lib("include/eunit/include/eunit.hrl").
+
+prepare() ->
+    C = db:mongo_connect(),
+    db:mongo_insert(C, test_lasso, urls, list_to_binary("www.bbc.co.uk/news")),
+    db:mongo_insert(C, test_lasso, urls, list_to_binary("")),
+    db:mongo_insert(C, test_lasso, urls, list_to_binary("www.bbc.co.uk/news")).
