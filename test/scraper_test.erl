202a8d254aa6d52217a62d68dd2602c43ea39874
Sasan <siesonz@gmail.com>
2012-03-22 17:37:38 -0700

    smal change

--- test/scraper_test.erl
+++ test/scraper_test.erl
@@ -4,6 +4,7 @@
 
 prepare() ->
     C = db:mongo_connect(),
+    
     db:mongo_insert(C, test_lasso, urls, list_to_binary("www.bbc.co.uk/news")),
-    db:mongo_insert(C, test_lasso, urls, list_to_binary("")),
-    db:mongo_insert(C, test_lasso, urls, list_to_binary("www.bbc.co.uk/news")).
+    db:mongo_insert(C, test_lasso, urls, list_to_binary("http://www.reddit.com/")),
+    db:mongo_insert(C, test_lasso, urls, list_to_binary("http://news.ycombinator.com/")).
